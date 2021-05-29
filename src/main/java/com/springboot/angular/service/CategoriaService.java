56260a1de2de63aa826b2215eafd47806ab476b3
André marques de lima <andre.marques091@gmail.com>
2021-05-29 19:08:04 -0300

    ajuste de pagination
    ajuste na implementação do pagination

--- src/main/java/com/springboot/angular/service/CategoriaService.java
+++ src/main/java/com/springboot/angular/service/CategoriaService.java
@@ -5,12 +5,16 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.springboot.angular.domain.Categoria;
 import com.springboot.angular.repository.CategoriaRepository;
 import com.springboot.angular.service.exception.IdNaoEncontrado;
 import com.springboot.angular.service.exception.NaoPodeDeletarId;
+
 
 @Service
 public class CategoriaService {
@@ -39,7 +43,7 @@ public class CategoriaService {
 	}
 	
 	public List<Categoria> Todos() {
-			return service.findAll();
+			return (List<Categoria>) service.findAll();
 	}
 	
 	public void Deletar(Integer id) {
@@ -50,6 +54,12 @@ public class CategoriaService {
 			throw new NaoPodeDeletarId("Não é possivel excluir uma categoria que possui produtos");
 		}
 	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return service.findAll(pageRequest);
+	}
+	
 }
 
 
